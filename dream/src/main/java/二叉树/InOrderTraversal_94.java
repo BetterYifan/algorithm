9ab317de2fc9458cd01fa0d353cb7522991bc89e
BetterYifan <1197688675@qq.com>
2023-03-09 22:58:52 +0800

    94

--- dream/src/main/java/二叉树/InOrderTraversal_94.java
+++ dream/src/main/java/二叉树/InOrderTraversal_94.java
@@ -2,6 +2,7 @@ package 二叉树;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 二叉树的中序遍历
@@ -21,6 +22,38 @@ public class InOrderTraversal_94 {
         inorder(root.left, list);
         list.add(root.val);
         inorder(root.right, list);
+    }
+
+    /**
+     * 迭代法
+     */
+    private static List<Integer> inorderTraversalV2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                result.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        InOrderTraversal_94 inOrderTraversal_94 = new InOrderTraversal_94();
+        TreeNode left = new TreeNode(4);
+        TreeNode right = new TreeNode(3);
+        TreeNode root = new TreeNode(5, left, right);
+        inorderTraversalV2(root);
+
     }
 
 
